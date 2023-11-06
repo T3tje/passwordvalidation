b03439ff1c02e9c80a661802bf3af2e06a83555a
teresa <teresa.juengel@posteo.de>
2023-11-06 10:59:58 +0100

    add more tests for pw-length

--- src/test/java/org/example/pwValidatorTest.java
+++ src/test/java/org/example/pwValidatorTest.java
@@ -17,4 +17,37 @@ class PwValidatorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void passwordLengthCheckTest_whenPwLengthIs10_returnTrue() {
+        //GIVEN
+        String password = "1234567890";
+        boolean expected = true;
+        //WHEN
+        boolean actual = PwValidator.passwordLengthCheck(password);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void passwordLengthCheckTest_whenPwLengthIs8_returnTrue() {
+        //GIVEN
+        String password = "12345678";
+        boolean expected = true;
+        //WHEN
+        boolean actual = PwValidator.passwordLengthCheck(password);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void passwordLengthCheckTest_whenPwLengthIs4_returnFalse() {
+        //GIVEN
+        String password = "abcd";
+        boolean expected = false;
+        //WHEN
+        boolean actual = PwValidator.passwordLengthCheck(password);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
 }
